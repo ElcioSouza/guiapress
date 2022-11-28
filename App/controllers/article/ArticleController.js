e9bfb2aa163b470f5f2ea724cb010c3ee735d0ee
Elcio de Souza Monico <elcio.monico@gmail.com>
2022-11-27 22:05:40 -0300

    artigo cadastro, deletar e painel de postagem de artigo feito

--- App/controllers/article/ArticleController.js
+++ App/controllers/article/ArticleController.js
@@ -1,12 +1,60 @@
 const express = require("express");
 const Router = express.Router();
+const Category = require("../../models/categories/CategoryModel");
+const Article = require("../../models/articles/ArticleModel");
+const slugify = require("slugify");
 
-Router.get('/articles', (req, res) => {
-       res.send("rota de artigo");
+Router.get('/admin/articles', (req, res) => {
+    // na buscar de artigo quero voce inclua a tabela categoria tambem ou seja join - include: [{model: Category}]
+      Article.findAll({
+         include: [{model: Category}]
+      }) // incluindo meu model de categoria
+             .then(articles => {
+                res.render("admin/articles/index",{articles})
+             });
 })
 
 Router.get('/admin/articles/new', (req, res) => {
-    res.send("rota para criar novo artigo");
+    Category.findAll().then(categories => {
+        res.render("admin/articles/new",{categories})
+    })
 })
+
+Router.post("/article/save", (req, res) => {
+    let title = req.body.title;
+    let body = req.body.body;
+    let category = req.body.category;
+
+    Article.create({
+        title,
+        slug: slugify(title),
+        body,
+        categoryId: category
+    }).then(() => {
+        res.redirect("/admin/articles")
+    })
+})
+
+Router.post('/articles/delete', (req, res) => {
+    
+    let id = req.body.id;
+
+    if(id){
+
+       if(!isNaN(id)) { // se id nao é um numero vai verificar
+            Article.destroy({
+                where: {
+                    id: id
+                }
+            }).then(() => {
+                res.redirect("/admin/articles");
+            })
+       } else {
+          res.redirect("/admin/articles");
+       }
+    } else {
+        res.redirect("/admin/articles");
+    }
+});
 
 module.exports = Router;
--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,41 @@
-alert("Meu site")
+    const Events = {
+        init: function(){
+            let formArticle = document.querySelectorAll(".delete-article");
+            let formCategory = document.querySelectorAll(".delete-category");
+            formArticle.forEach(article => {
+                if(article) {
+                    article.addEventListener('submit', function (e) {
+                        e.preventDefault();
+                        let decision = confirm("Você deseja deletar este artigo ?");
+                        if (decision) {
+                            article.submit();
+                        } else {
+                            console.log("Não quero deletar, foi um acidente!")
+                        }
+                    })
+                }
+            })
+
+            formCategory.forEach(category => {
+                if(category) {
+                    category.addEventListener('submit', function (e) {
+                        e.preventDefault();
+                        let decision = confirm("Você deseja deletar está categoria ?");
+                        if (decision) {
+                            category.submit();
+                        } else {
+                            console.log("Não quero deletar, foi um acidente!")
+                        }
+                    })
+                }
+            })
+        }
+    }
+    const App = {
+        init: function(){
+            Events.init();
+        }
+    }
+    document.addEventListener("DOMContentLoaded", function (event) {
+        App.init();
+    });
